9de114d6fd592a7bec6c1493013f24aa390d7b25
Henrick <ricknogueira1231@gmail.com>
2025-04-17 08:50:37 -0300

    10.4. Implementando a persistência com Jakarta Persistence e Spring Data JPA

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -3,18 +3,24 @@ package com.algaworks.algasensors.device.management.api.controller;
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/sensors")
 public class SensorController {
+
+    private final SensorRepository sensorRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Sensor create(@RequestBody SensorInput input) {
-        return Sensor.builder()
-                .id(IdGenerator.generateTSID())
+        Sensor sensor = Sensor.builder()
+                .id(new SensorId(IdGenerator.generateTSID()))
                 .name(input.getName())
                 .ip(input.getIp())
                 .location(input.getLocation())
@@ -22,6 +28,8 @@ public class SensorController {
                 .model(input.getModel())
                 .enabled(false)
                 .build();
+
+        return sensorRepository.saveAndFlush(sensor);
     }
 
 }
--- src/main/java/com/algaworks/algasensors/device/management/domain/model/Sensor.java
+++ src/main/java/com/algaworks/algasensors/device/management/domain/model/Sensor.java
@@ -1,18 +1,24 @@
 package com.algaworks.algasensors.device.management.domain.model;
 
-import io.hypersistence.tsid.TSID;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Sensor {
 
-    private TSID id;
+    @Id
+    @AttributeOverride(name = "value", column = @Column(name = "id", columnDefinition = "BIGINT"))
+    private SensorId id;
     private String name;
     private String ip;
     private String location;
