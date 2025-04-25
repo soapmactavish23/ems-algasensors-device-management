b1a100dd188beff2e310c771aebae2133a79fa14
Henrick <ricknogueira1231@gmail.com>
2025-04-24 22:14:30 -0300

    11.6. Implementando consulta de dados usando GET

--- src/main/java/com/algaworks/algasensors/device/management/api/client/SensorMonitoringClient.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/SensorMonitoringClient.java
@@ -1,8 +1,10 @@
 package com.algaworks.algasensors.device.management.api.client;
 
+import com.algaworks.algasensors.device.management.api.model.SensorMonitoringOutput;
 import io.hypersistence.tsid.TSID;
 
 public interface SensorMonitoringClient {
     void enableMonitoring(TSID sensorId);
     void disableMonitoring(TSID sensorId);
+    SensorMonitoringOutput getDetail(TSID sensorId);
 }
--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algasensors.device.management.api.controller;
 
 import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClient;
+import com.algaworks.algasensors.device.management.api.model.SensorDetailOutput;
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
+import com.algaworks.algasensors.device.management.api.model.SensorMonitoringOutput;
 import com.algaworks.algasensors.device.management.api.model.SensorOutput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
@@ -30,12 +32,26 @@ public class SensorController {
         return sensors.map(this::convertToModel);
     }
 
-    @GetMapping("{sensorId}")
+    @GetMapping("/{sensorId}")
     public SensorOutput getOne(@PathVariable TSID sensorId) {
         Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         return convertToModel(sensor);
+    }
+
+    @GetMapping("/{sensorId}/detail")
+    public SensorDetailOutput getOneWithDetail(@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        SensorMonitoringOutput monitoringOutput = sensorMonitoringClient.getDetail(sensorId);
+        SensorOutput sensorOutput = convertToModel(sensor);
+
+        return SensorDetailOutput.builder()
+                .monitoring(monitoringOutput)
+                .sensor(sensorOutput)
+                .build();
     }
 
     @PostMapping
